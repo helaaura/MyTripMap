e17d58d2c79a91bc5c3a8e976c00510e60f29875
Salim <solsalim@gmail.com>
2014-11-01 11:37:06 +0100

    - pout a rounded map (commit only the js, css and html)

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,58 +1,62 @@
-angular.module("angular-google-maps-sol", ["google-maps".ns()])
-
-.config(['GoogleMapApiProvider'.ns(), function(GoogleMapApi) {
+var app = angular.module("my-trip-maps", ["google-maps".ns()]).config(['GoogleMapApiProvider'.ns(),
+function(GoogleMapApi) {
 	GoogleMapApi.configure({
-		//    key: 'AIzaSyB8RMXnE6nvNAklrNaxlBWBqQoD3LZtJ9I,
+		key : 'AIzaSyB8RMXnE6nvNAklrNaxlBWBqQoD3LZtJ9I',
 		v : '3.17',
 		libraries : 'weather,geometry,visualization'
 	});
-}])
-
-.controller("MyMapController", ['$scope', 'Logger'.ns(), 'GoogleMapApi'.ns(), function($scope, $log, GoogleMapApi) {
-	GoogleMapApi.then(function(maps) {
-		this.googleVersion = maps.version;
-		maps.visualRefresh = true;
-		console.log('this.map.rectangle.bounds set');
-	});
-
-	angular.extend($scope, {
-		map : {
-			show : true,
-			control : {},
-			version : "uknown",
-			heatLayerCallback : function(layer) {
-				//set the heat layers backend data
-				var mockHeatLayer = new MockHeatLayer(layer);
-			},
-			showTraffic : true,
-			showBicycling : false,
-			showWeather : false,
-			showHeat : false,
-			center : {
-				latitude : 45,
-				longitude : 10
-			},
-			options : {
-				streetViewControl : false,
-				panControl : false,
-				maxZoom : 20,
-				minZoom : 3
-			},
-			zoom : 3,
-
-			events : {
-				tilesloaded : function(map, eventName, originalEventArgs) {
-				},
-				click : function(mapModel, eventName, originalEventArgs) {
-					// 'this' is the directive's
-					$log.info("user defined event: " + eventName, mapModel, originalEventArgs);
-
-					var e = originalEventArgs[0];
-					var lat = e.latLng.lat(), lon = e.latLng.lng();
-					console.log("lat:" + lat + ", long:" + lon);
-				}
-			},
-		}
-	});
 }]);
 
+app.controller("tripMapController", ["Logger".ns(), "GoogleMapApi".ns(),
+function(log, GoogleMapApi) {
+	log.doLog = true;
+	var tripMapControllerScope = this;
+	GoogleMapApi.then(function(maps) {
+		log.info("google map sdk loaded");
+		//TOSEE i don't knwo what this do 
+		// tripMapControllerScope.map.control.refresh({
+			// latitude : 32.779680,
+			// longitude : -79.935493
+		// });
+		//TOSEE i don't knwo what this do
+		// var theMap = tripMapControllerScope.map.control.getGMap();
+		// maps.visualRefresh = true;
+		//TOSEE i don't knwo what this do
+		// tripMapControllerScope.map.rectangle.bounds = new maps.LatLngBounds(new maps.LatLng(55, -100), new maps.LatLng(49, -78));
+	});
+
+	this.map = {
+		center : {
+			latitude : 36,
+			longitude : 10
+		},
+		zoom : 3,
+		width : '500',
+		height : '500',
+		events : {
+			click : function(maps, eventName, arguments) {
+				log.info("map clicked in:" + arguments[0].latLng.lat() + ", " + arguments[0].latLng.lng());
+			},
+		},	//TOSEE i don't knwo what this do
+		control : {
+			refresh : function() {
+				log.debug("user control refresh");
+			},
+			getGMap : function() {
+				log.debug("user control getGMap");
+				return tripMapControllerScope.map;
+			}
+		},
+		options: {
+	        // streetViewControl: false,
+	        // panControl: false,
+	        maxZoom: 20,
+	        minZoom: 3
+      	}
+	};
+}]);
+
+//TOSEE
+// log.info(map);
+// log.debug(uuid);
+// log.error(mapInstanceNumber);
