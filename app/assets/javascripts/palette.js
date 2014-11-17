3fac7d82c83a675f2ecd21742a4708134c5ed83c
Salim <solsalim@gmail.com>
2014-11-17 13:42:18 +0100

    put album/image directives position

--- app/assets/javascripts/palette.js
+++ app/assets/javascripts/palette.js
@@ -7,10 +7,16 @@ app.directive("palette", function() {
 		controller: function($scope) {
 			this.isFolded = false;
 			this.changeFolding = function() {
+				console.log($scope.type);
+				console.log($scope.type === 'album');
+				console.log($scope.type === 'image');
 				this.isFolded = !this.isFolded;
 			};
+			this.isForAlbum = $scope.type == 'album';
+			this.isForImage = $scope.type == 'image';
+			
 		},
-		controllerAs: "foldingCtrl",
+		controllerAs: "paletteCtrl",
 		transclude: true,
 		scope: {
 			type: '@'
